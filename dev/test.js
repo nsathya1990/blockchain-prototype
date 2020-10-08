524a96cf003464b73149f1f97b3045ea23747dfb
unknown <nsathya1990@gmail.com>
2020-10-08 22:12:15 +0530

    testing hash block method

--- dev/test.js
+++ dev/test.js
@@ -1,8 +1,25 @@
 const Blockchain = require('./blockchain');
-
 const bitcoin = new Blockchain();
 
-bitcoin.createNewBlock(2389, '0INA90SDNF90N', '90ANSD9F0N9009N');
+const previousBlockHash = '01KDHJKBCJKSFFKJSH';
+const currentBlockData = [{
+    amount: 10,
+    sender: 'NSKNDJKBJKBFK',
+    recipient: 'SKNJFBJKFBKJFB'
+}, {
+    amount: 30,
+    sender: 'DKJDBJKDJKBJKBFK',
+    recipient: 'SBDLIDIBJKFBKJFB'
+}, {
+    amount: 200,
+    sender: 'HEIEUEKBJKBFK',
+    recipient: 'SBSJKJKFBKJFB'
+}];
+const nonce = 100;
+
+console.log(bitcoin.hashBlock(previousBlockHash, currentBlockData, nonce));
+
+/* bitcoin.createNewBlock(2389, '0INA90SDNF90N', '90ANSD9F0N9009N');
 
 bitcoin.createNewTransaction(100, 'SATH89AHJGHJVFJFV', 'VICKNMUJKJDDIUDWQAXK');
 
@@ -14,4 +31,4 @@ bitcoin.createNewTransaction(2000, 'SATH89AHJGHJVFJFV', 'VICKNMUJKJDDIUDWQAXK');
 
 bitcoin.createNewBlock(98709087, 'AJHJKDGHJHD', 'IOYSKLSGHV');
 
-console.log(bitcoin.chain[2]);
+console.log(bitcoin.chain[2]); */
