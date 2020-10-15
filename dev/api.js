4e60954ba37d6c57af2679383ab381e474e62b1f
unknown <nsathya1990@gmail.com>
2020-10-15 10:58:13 +0530

    building the get mine endpoint

--- dev/api.js
+++ dev/api.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const Blockchain = require('./blockchain');
+const uuid = require('uuid/v1');
+
+const nodeAddress = uuid().split('-').join('');
 
 const bitcoin = new Blockchain();
 
@@ -18,7 +21,22 @@ app.post('/transaction', function (req, res) {
 });
 
 app.get('/mine', function (req, res) {
+    const lastBlock = bitcoin.getLastBlock();
+    const previousBlockHash = lastBlock['hash'];
+    const currentBlockData  = {
+        transactions: bitcoin.pendingTransactions,
+        index: lastBlock['index'] + 1
+    };
+    const nonce = bitcoin.proofOfWork(previousBlockHash, currentBlockData);
+    const blockHash = bitcoin.hashBlock(previousBlockHash, currentBlockData, nonce);
 
+    bitcoin.createNewTransaction(12.5, '00', nodeAddress); // mining reward
+
+    const newBlock = bitcoin.createNewBlock(nonce, previousBlockHash, blockHash);
+    res.json({
+        note: 'New block mined successfully',
+        block: newBlock
+    });
 });
 
 app.listen(3000, function () {
