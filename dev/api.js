9dba13695dda2e368da29dc4d922055ee82c719a
unknown <nsathya1990@gmail.com>
2020-10-15 17:51:59 +0530

    adding uuid package for creating addresses

--- dev/api.js
+++ dev/api.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const Blockchain = require('./blockchain');
-const uuid = require('uuid/v1');
+const uuid = require('uuid').v1;
 
 const nodeAddress = uuid().split('-').join('');
 
