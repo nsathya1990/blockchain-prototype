9831ce1501d517237f806cc67ce50c9f8157c19a
unknown <nsathya1990@gmail.com>
2021-01-02 10:24:59 +0530

    get block method

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -92,8 +92,18 @@ Blockchain.prototype.chainIsValid = function (blockchain) {
     if (!correctNonce || !correctPreviousBlockHash || !correctHash || !correctTransactions) {
         validChain = false;
     }
- 
+
     return validChain;
 };
+
+Blockchain.prototype.getBlock = function (blockHash) {
+    let correctBlock = null;
+    this.chain.forEach(block => {
+        if (block.hash === blockHash) {
+            correctBlock = block;
+        }
+    });
+    return correctBlock;
+}
 
 module.exports = Blockchain;
