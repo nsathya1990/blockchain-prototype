12461fb4515f2cb503771b008321d72fba4d3c70
unknown <nsathya1990@gmail.com>
2021-01-02 15:45:32 +0530

    building GET /address/:address endpoint

--- dev/networkNode.js
+++ dev/networkNode.js
@@ -232,6 +232,11 @@ app.get('/transaction/:transactionId', function (req, res) {
 });
 
 app.get('/address/:address', function (req, res) {
+    const address = req.params.address;
+    const addressData = bitcoin.getAddressData(address);
+    res.json({
+        addressData: addressData
+    });
 });
 
 
